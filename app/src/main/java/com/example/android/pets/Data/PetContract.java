44f516574b413b62ceccba40426f71b0fb836e3e
emperoralz97 <emperorabbas@gmail.com>
2018-05-19 13:36:20 +0530

    lesson 2: Intro to Content Provider (content uri)

--- app/src/main/java/com/example/android/pets/Data/PetContract.java
+++ app/src/main/java/com/example/android/pets/Data/PetContract.java
@@ -1,14 +1,22 @@
 package com.example.android.pets.Data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
 
     private PetContract(){}
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
 
     public static class PetEntry implements BaseColumns{
         //Constant for table name
         public static final String TABLE_NAME = "pets";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+
 
         //Constant for header column
         public static final String _ID = BaseColumns._ID;
